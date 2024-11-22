4acadc3896e5ab9bfebeae741f723fb1db17dd91
Bruno Nogueira Burian <151679524+BruBurian@users.noreply.github.com>
2024-11-22 20:19:30 -0300

    Add files via upload

---
+++ AppData/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using LessPollu.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LessPollu.MVC.AppData
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<UsuarioEntity> Usuario { get; set; }
+        public DbSet<RelatorioEntity> Relatorio { get; set; }
+        public DbSet<IndustriaEntity> Industria { get; set; }
+
+    }
+}
